4aeadd52afe3a290ffc99c007d3945152ef5ffb7
Emmanuel Leyan <emmanuel.leyan@gmail.com>
2022-04-09 12:10:16 +0000

    glow effect added

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 import "../../assets/styles/index.css";
 
 const Home = () => {
-	const [color, setColor] = useState("red");
+	const defaultColors = ["red", "yellow", "green"];
+
+	const [ActiveColor, setActiveColor] = useState("");
 
 	return (
 		<div>
@@ -12,9 +14,36 @@ const Home = () => {
 					<div className="d-flex flex-column align-items-center">
 						<div className="container-support bg-black"></div>
 						<div className="container-lights d-flex flex-column justify-content-evenly align-items-center bg-black">
-							<div className="light red"></div>
-							<div className="light yellow"></div>
-							<div className="light green"></div>
+							<div
+								onClick={() => {
+									setActiveColor = "red";
+								}}
+								className={
+									"light red" +
+									(ActiveColor === "red"
+										? " glow-effect"
+										: "")
+								}></div>
+							<div
+								onClick={() => {
+									setActiveColor = "yellow";
+								}}
+								className={
+									"light yellow" +
+									(ActiveColor === "yellow"
+										? " glow-effect"
+										: "")
+								}></div>
+							<div
+								onClick={() => {
+									setActiveColor = "green";
+								}}
+								className={
+									"light green" +
+									(ActiveColor === "green"
+										? " glow-effect"
+										: "")
+								}></div>
 						</div>
 					</div>
 				</div>
